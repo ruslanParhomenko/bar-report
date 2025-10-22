e1a15ef518088a7fdb07bb098bb71ba36d8617f0
nova <cng.it.department@gmail.com>
2025-10-22 21:56:10 +0300

    fix user action

--- src/app/actions/users/getUsers.ts
+++ src/app/actions/users/getUsers.ts
@@ -16,6 +16,6 @@ export const getUsers = unstable_cache(fetchUsers, ["users"], {
   tags: ["users"],
 });
 
-export const invalidateUsers = () => {
+export async function invalidateUsers ()  {
   revalidateTag("users");
 };
