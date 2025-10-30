c0ea57c72630d317e67db8e2c7497b6a378b07d5
NBM <cng.it.department@gmail.com>
2025-10-30 14:04:05 +0200

    revalidate

--- src/app/actions/schedule/invalidateScheduleEverywhere.ts
+++ src/app/actions/schedule/invalidateScheduleEverywhere.ts
@@ -3,9 +3,11 @@
 import { revalidateTag } from "next/cache";
 
 export async function invalidateScheduleEverywhere(tag: string) {
+  console.log(tag);
   revalidateTag(tag);
 
   const endpoints = [
+    "http://localhost:3000/api/revalidate",
     "https://report-bar-n.netlify.app/api/revalidate",
     "https://bar-report2.vercel.app/api/revalidate",
     "https://bar-report-rus.vercel.app/api/revalidate",
