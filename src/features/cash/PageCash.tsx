c8d5caec9a7b1cbc691e529f5e0c62f99e19e729
NBM <cng.it.department@gmail.com>
2025-11-06 16:52:28 +0200

    add f calcul salary

--- src/features/cash/PageCash.tsx
+++ src/features/cash/PageCash.tsx
@@ -79,7 +79,7 @@ export function PageCash({ initialData }: { initialData: any[] }) {
         onKeyDown={(e) => e.key === "Enter" && e.preventDefault()}
         className="flex flex-col"
       >
-        <FilterDataByMonth isDisabled={isDisabled} />
+        <FilterDataByMonth disabled={isDisabled} />
         <Table className="md:table-fixed">
           <TableHeaderCash monthDays={monthDays} month={month} />
           <TableBodyCash
