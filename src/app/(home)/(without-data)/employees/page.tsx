678fc8367ad4852fe9104c3905f29fb184803aaf
NBM <cng.it.department@gmail.com>
2025-11-29 15:16:28 +0200

    refact employee

--- src/app/(home)/(without-data)/employees/page.tsx
+++ src/app/(home)/(without-data)/employees/page.tsx
@@ -6,9 +6,9 @@ export default async function Page({
 }: {
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }) {
-  const { tabs, role } = await searchParams;
+  const { role } = await searchParams;
   const data = await getEmployees();
   const employees =
     role === "all" ? data : data.filter((e: any) => e.role === role);
-  return <EmployeesPage tabs={tabs as string} employees={employees as any} />;
+  return <EmployeesPage employees={employees as any} />;
 }
