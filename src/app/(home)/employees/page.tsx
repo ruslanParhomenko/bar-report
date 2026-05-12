fa38b489cab436f89bb55e88fd59cbb9b45b88aa
NBM <cng.it.department@gmail.com>
2026-05-12 17:04:24 +0300

    fix

--- src/app/(home)/employees/page.tsx
+++ src/app/(home)/employees/page.tsx
@@ -4,9 +4,10 @@ import { headers } from "next/headers";
 export default async function Page({
   searchParams,
 }: {
-  searchParams: Promise<{ [key: string]: string | undefined }>;
+  searchParams: Promise<{ [key: string]: string }>;
 }) {
   const { tab, filter } = await searchParams;
+  if (!tab || !filter) return null;
   const headerStore = await headers();
   const isAdmin = headerStore.get("x-is-admin") === "true";
   return <EmployeesPage isAdmin={isAdmin} tab={tab} filter={filter} />;
