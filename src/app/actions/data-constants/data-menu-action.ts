a356672592fd2cafac6be186a95e84f7c74b3ca0
NBM <cng.it.department@gmail.com>
2026-07-22 16:44:28 +0300

    fix

--- src/app/actions/data-constants/data-menu-action.ts
+++ src/app/actions/data-constants/data-menu-action.ts
@@ -1,6 +1,7 @@
 import { DATA_MENU_ACTION_TAG } from "@/constants/action-tag";
 import { dbAdmin } from "@/lib/firebase-admin";
 import { unstable_cache, updateTag } from "next/cache";
+import { invalidateEverywhere } from "../invalidateEverywhere/invalidate-everywhere";
 
 const actionTag = DATA_MENU_ACTION_TAG;
 
@@ -13,41 +14,6 @@ export type MenuItem = {
   label?: string;
 };
 
-// export type MenuSubgroup = {
-//   label: string;
-//   items: MenuItem[];
-// };
-
-// export type MenuSection = {
-//   id: string;
-//   title: string;
-//   items?: MenuItem[];
-//   subgroups?: MenuSubgroup[];
-// };
-
-// export type MenuColumn = {
-//   id: string;
-//   title?: string;
-
-//   sections?: MenuSection[];
-
-//   twoColumnLayout?: boolean;
-//   columnLeft?: MenuSection[];
-//   columnRight?: MenuSection[];
-
-//   type?: "cover";
-//   qrUrl?: string;
-
-//   note?: string;
-// };
-
-// export type MenuPage = {
-//   name: string;
-//   price: number;
-//   weight: string;
-//   label?: string;
-// };
-
 export type MenuDataType = Record<string, MenuItem[]>;
 
 // CREATE
@@ -58,6 +24,7 @@ export async function createDataMenu(data: MenuDataType) {
   await docRef.set(data);
 
   updateTag(actionTag);
+  await invalidateEverywhere(actionTag);
 }
 
 // GET
--- src/app/actions/invalidateEverywhere/invalidate-everywhere.ts
+++ src/app/actions/invalidateEverywhere/invalidate-everywhere.ts
@@ -1,14 +1,21 @@
 "use server";
 
 export async function invalidateEverywhere(tag: string) {
-  const url = "https://schedule-nuovo.vercel.app/api/revalidate";
+  const urls = [
+    "https://schedule-nuovo.vercel.app/api/revalidate",
+    "https://menu-vip.vercel.app/api/revalidate",
+  ];
 
-  await fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "x-revalidate-secret": process.env.REVALIDATE_SECRET!,
-    },
-    body: JSON.stringify({ tag }),
-  }).catch(() => {});
+  await Promise.all(
+    urls.map((url) =>
+      fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "x-revalidate-secret": process.env.REVALIDATE_SECRET!,
+        },
+        body: JSON.stringify({ tag }),
+      }).catch(() => {}),
+    ),
+  );
 }
